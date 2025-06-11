25ae3784d3d93e707b4bfd5f0b4ee5045c64d8b6
MYHan531 <mngynhn@gmail.com>
2025-06-11 16:22:36 +0800

    updated constants.py

---
+++ utils/constants.py
@@ -0,0 +1,10 @@
+import configparser
+import os
+
+parser = configparser.ConfigParser()
+parser.read(os.path.join(os.path.dirname(__file__), '../config/config.conf'))
+
+SECRET = parser.get('api_keys', 'reddit_secret_key')
+CLIENT_ID = parser.get('api_keys', 'reddit_client_id')
+
+DATABASE_HOST = parser.get('database', 'database_host')
